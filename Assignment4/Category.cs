8e054f06b8930f9381cac25d3122a416f0c0951d
Mads Jönsson <madsjonsson1997@gmail.com>
2021-10-26 23:01:04 +0200

    put it all together

---
+++ Assignment4/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Npgsql;
+
+namespace Assignment4
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description  { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id = {Id}, Name = {Name}";
+        }
+    }
+
+
+    
+}
---
+++ Assignment4/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment4
+{
+    class Class1
+    {
+    }
+
+//    public class AdoProgram
+//    {
+//        static void Main(string[] args)
+//        {
+//            var connStr = "host=localhost;db=northwind;uid=bulskov;pwd=henrik";
+
+//            var conn = new NpgsqlConnection(connStr);
+//            conn.Open();
+
+//            var cmd = conn.CreateCommand();
+//            cmd.CommandText = "select * from categories";
+//            cmd.Connection = conn;
+
+//            var reader = cmd.ExecuteReader();
+
+//            while (reader.Read())
+//            {
+//                var category = new Category
+//                {
+//                    Id = reader.GetInt32(0),
+//                    Name = reader.GetString(1),
+//                    Decription = reader.GetString(2)
+//                };
+//                Console.WriteLine(category);
+//            }
+
+//        }
+//    }
+//}
+}
